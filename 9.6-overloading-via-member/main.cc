1d82e37b54fe68e009eaa639acb1c672bfb8016e
Thorsten Lorenz <thlorenz@gmx.de>
2014-03-25 09:41:39 -0400

    wip

---
+++ 9.6-overloading-via-member/main.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+
+class Cents {
+private:
+  int _cents;
+public:
+  Cents(int cents) { _cents = cents; }
+
+  int cents() { return _cents; }
+
+  Cents operator-() {
+    return Cents(-_cents);
+  }
+};
+
+
+int main(void) {
+  using namespace std;
+
+  Cents cents(20);
+  cout << cents.cents() << endl;
+  cout << (cents - Cents(10)).cents() << endl;
+
+  return 0;
+}
